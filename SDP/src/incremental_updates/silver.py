0a7b87f1883bef090d79089c9e9e8e2818550da7
sanumolu_cavallo <sanumolu@cavallotechnologies.ca>
2026-03-13 15:16:50 -0700

    minor changes

--- SDP/src/incremental_updates/silver.py
+++ SDP/src/incremental_updates/silver.py
@@ -3,15 +3,14 @@ from pyspark import pipelines as dp
 from pyspark.sql import functions as F
 
 # COMMAND ----------
-# create the table
+dp.view
+def customers_bronze():
+    spark.readStream.table("my_projects_dev.cutsomers_bronze.cutsomes")
+
 dp.create_streaming_table(
     name="customer_history",
     commenet= "SCD2 for customers"
 )
-
-dp.view
-def customers_bronze():
-    spark.readStream.table("my_projects_dev.cutsomers_bronze.cutsomes")
 
 dp.create_auto_cdc_flow(
     target = "my_projects_dev.cutsomers_silver.cutsomer_history",
