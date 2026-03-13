a69ae2ff2ab9b86d999c40ce04ec7adbc9fe3d8a
sanumolu_cavallo <sanumolu@cavallotechnologies.ca>
2026-03-13 15:04:16 -0700

    minor change

--- SDP/src/incremental_updates/bronze.py
+++ SDP/src/incremental_updates/bronze.py
@@ -2,7 +2,6 @@
 from pyspark.sql.types import *
 
 # COMMAND ----------
-
 df = spark.createDataFrame([
         {"id": 1, "name": "Alice","city": "NYC", "timestamp": "2022-01-01"},
         {"id": 2, "name": "Bob", "city": "SFO", "timestamp": "2022-01-01"},
@@ -13,5 +12,4 @@ df = spark.createDataFrame([
 df.write.mode("append").saveAsTable("my_projects_dev.cutsomers_bronze.cutsomers")
 
 # COMMAND ----------
-
 # Add new day in append mode
